af131e467c88148a34817e2ece0dd27ea75c9541
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:59:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
-    kats_deli.each_with_index do |name, index|
+    katz_deli.each_with_index do |name, index|
       line_array << ("#{index + 1}. #{name}")
     end 
     puts "The line is currently: #{line_array.join(" ")}"
