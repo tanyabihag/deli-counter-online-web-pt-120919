4beb85df15fc2245ffdef4503b1f43dd0ffd2955
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:39:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ kats_deli = []
 
 def line(kats_deli)
   line_array = []
-  if kats_deli == 0 
+  if kats_deli.size == 0 
     puts "The line is currently empty."
   else 
     kats_deli.each_with_index do |name, index|
