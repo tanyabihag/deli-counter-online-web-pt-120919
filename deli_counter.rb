425187f9a0ddaf74ddace5c05bb9f32336ec7db4
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:53:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
-kats_deli = []
+katz_deli = []
 
-def line(kats_deli)
+def line(katz_deli)
   line_array = []
-  if kats_deli.size == 0 
+  if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
     kats_deli.each_with_index do |name, index|
@@ -11,13 +11,18 @@ def line(kats_deli)
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-#def take_a_number(kats_deli, name)
   
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{katz_deli.last}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli)
+   line_array = []
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else 
     
+  
       
   
