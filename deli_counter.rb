947a6ef0305cc2477c9f5e20d0f1512632023ac5
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:57:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,10 +18,15 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-   line_array = []
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
+    katz_deli.each do |customer|
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+  katz_deli
+end
     
   
       
