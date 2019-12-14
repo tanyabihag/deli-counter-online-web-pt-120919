d50a3b86fae26e8b882c0adf3cc6e83304bfe864
Tanya Bihag <tbihag@gmail.com>
2019-12-14 04:26:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(customers)
 customers = []
 counter = 0 
 
-  if customers == 0 
+  if counter == 0 
     puts "The line is currently empty."
-  elsif customers.size == 0 
+  elsif customers.size > 0 
     customers.each do |customer|
     counter += 1 
     puts "The line is currently: #{counter}. + #{customer}"
